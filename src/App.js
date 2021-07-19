e7e641d4b2705c9ce096088c08f88a6d55c9ca77
anna-ciuvalscaia <67141302+anna-ciuvalscaia@users.noreply.github.com>
2021-07-19 18:26:23 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return <h1>First React JS App</h1>
+  }
+}
+
+
+// main App component
+/*function App() {
+  // below JSX
+  return (
+    <div className="App">
+      <h1>First React JS App</h1>
+    </div>
+  );
+}*/
+
+export default App;
